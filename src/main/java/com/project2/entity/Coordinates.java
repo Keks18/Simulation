7587e7e86281d5b25a6dd4391e14b3a32dda27f1
Max <myshailik@gmail.com>
2023-08-14 17:10:18 +0300

    refactor Coordinates class and Renderer in stable version 0.6 + setters and getters

--- src/main/java/com/project2/entity/Coordinates.java
+++ src/main/java/com/project2/entity/Coordinates.java
@@ -4,8 +4,8 @@ import java.util.Objects;
 import java.util.Random;
 
 public class Coordinates{
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public Coordinates() {
         this.x = generateRandomCoordinate();
@@ -25,6 +25,14 @@ public class Coordinates{
 
     public int getY() {
         return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     @Override
--- src/main/java/com/project2/view/Renderer.java
+++ src/main/java/com/project2/view/Renderer.java
@@ -3,12 +3,6 @@ package com.project2.view;
 import com.project2.SimulationMap;
 import com.project2.entity.Coordinates;
 import com.project2.entity.Entity;
-import com.project2.entity.Herbivore;
-import com.project2.entity.Predator;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class Renderer extends Entity {
     public SimulationMap simulationMap;
@@ -17,9 +11,8 @@ public class Renderer extends Entity {
     public void render(int size){
         Coordinates keyCoordinates = new Coordinates();
 
-
-        FIRST:for (keyCoordinates.y = 1; keyCoordinates.y < size; keyCoordinates.y++) {
-            SECOND:for (keyCoordinates.x = 1; keyCoordinates.x < size; keyCoordinates.x++) {
+        FIRST:for (keyCoordinates.setY(1); keyCoordinates.getY() < size; keyCoordinates.setY(keyCoordinates.getY()+1)) {
+            SECOND:for (keyCoordinates.setX(1); keyCoordinates.getX() < size; keyCoordinates.setX(keyCoordinates.getX()+1)) {
                 for (Coordinates c: simulationMap.getMap().keySet()) {
                     if (keyCoordinates.equals(c) && simulationMap.getMap().get(c) != null){
                         System.out.print("[" + simulationMap.getMap().get(c).view + "] ");
